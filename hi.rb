870af03d59cdb2bfd8d013380d53cda97928a066
Hiten Parmar <hiten@hbook.local>
2010-01-15 01:14:50 -0800

    next page, colors, utf8

--- hi.rb
+++ hi.rb
@@ -10,7 +10,9 @@ end
 
 get '/search' do
   @query = params[:query]
-  @results = search_truveo(@query)
+  @page  = params[:page]
+  @page  = 1 if @page.nil?
+  @results = search_truveo(@query, @page)
   
   @results.video_set.each do |vid|
     vid['thumbnailUrl'] = vid['thumbnailUrlLarge'] if !vid['thumbnailUrlLarge'].nil?
@@ -25,9 +27,22 @@ get '/*' do
   haml :search
 end
 
-def search_truveo(*query)
+def search_truveo(query, page=1, page_size=10)
   t = Truveo.new("08f3e5e3670ce918d")
   query = "nfl:season_type=POST" if query.nil?
-  return t.get_videos(query)
+  page = 1 if page.nil?
+  start =  (page.to_i * page_size.to_i) - 10
+  return t.get_videos(query, page_size, start)
 end
-  
+
+
+helpers do
+  # Generate a link path for :href
+  def l(*args)
+    args.compact!
+    query = args.pop if args.last.is_a?(Hash)
+    path = env["SCRIPT_NAME"] + "/" + args.map { |a| escape(a) }.join("/")
+    path << "?" << build_query(query) if query
+    path
+  end
+end
