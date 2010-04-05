f7aa15ffdc3f68f0f193fa74eb18406629913406
Hiten Parmar <hiten@hbook.local>
2010-04-05 09:13:52 -0700

    options!

--- hi.rb
+++ hi.rb
@@ -11,17 +11,31 @@ end
 
 get '/search' do
   @query = params[:query]
+  @query_modified = params[:query_modified]
   @page  = params[:page]
   @page  = 1 if @page.nil?
-  @results = search_truveo(@query, @page)
+  if @query_modified
+    full_query = @query + ' ' + @query_modified
+  else
+    full_query = @query
+  end
+  p full_query
+  @results = search_truveo(full_query, @page)
   
   @results.video_set.each do |vid|
-    # vid['thumbnailUrl'] = vid['thumbnailUrlLarge'] if !vid['thumbnailUrlLarge'].nil?
-    vid['dateFound'] = Time.parse(vid['dateFound']).strftime('%I:%M%p %b %d, %Y')
+    vid['thumbnailUrl'] = vid['thumbnailUrlLarge'] if !vid['thumbnailUrlLarge'].nil?
+    p vid['dateFound']
     vid_time = Time.parse(vid['dateFound'])
-    vid['dateFound'] = vid_time.strftime('%I:%M%p').downcase.gsub(/^0/, '')
+    vid['dateFound'] = vid_time.strftime('%I:%M%p').downcase.gsub(/^0/, '').downcase
     vid['dateFound'] << vid_time.strftime(' %b %d, %Y')
-    vid['runtime'] = (vid['runtime'].to_i / 60).to_s + ':' + (vid['runtime'].to_i % 60).to_s if vid['runtime']
+    if vid['runtime']
+      minutes = (vid['runtime'].to_i / 60)
+      seconds = (vid['runtime'].to_i % 60)
+      minutes = minutes > 0 ? minutes.to_s + " min" : ""
+      seconds = seconds > 0 ? seconds.to_s + " sec" : ""
+      vid['runtime'] = (minutes + " " + seconds).chomp
+    end
+    
     
     
     # vid['description'].gsub!(@query, '<strong>#{@query}</strong>') unless vid['description'].nil?
@@ -33,20 +47,17 @@ get '/search' do
 end
 
 get '/*' do
-  @query = "Daft Punk"
-  @query = nil
-  # @results = search_truveo(@query)
+  @query, @results = nil, nil
   haml :search
 end
 
 def search_truveo(query, page=1, page_size=10)
   t = Truveo.new("08f3e5e3670ce918d")
-  query = "nfl:season_type=POST" if query.nil?
+  # query = "nfl:season_type=POST" if query.nil?
   page = 1 if page.nil?
   start =  (page.to_i * page_size.to_i) - 10
   return t.get_videos(query, page_size, start)
 end
-
 
 helpers do
   # Generate a link path for :href
