de66fc6b5a6ef8cc63f5897044b67992ef9f08fb
Hiten Parmar <hiten@hbook.local>
2010-04-05 09:25:24 -0700

    options!

---
+++ public/yum.js
@@ -0,0 +1,42 @@
+var Yum = {
+  // Ripped from hurl.it
+  // apply label hints to inputs based on their title attribute
+  labelHints: function(el) {
+    $(el).each(function() {
+      var self = $(this), title = self.attr('title')
+ 
+      // indicate inputs using defaults
+      self.addClass('defaulted')
+ 
+      if (self.val() === '' || self.val() === title) {
+        self.val(title).css('color', '#aaa')
+      } else {
+        self.addClass('focused')
+      }
+ 
+      self.focus(function() {
+        if (self.val() === title) {
+          self.val('').addClass('focused').css('color', '#333')
+        }
+      })
+ 
+      self.blur(function() {
+        if (self.val() === '') {
+          self.val(title).removeClass('focused').css('color', '#aaa')
+        }
+      })
+    })
+  }
+}
+
+$(document).ready(function() {
+  // in-field labels
+  Yum.labelHints('input');
+  
+  $('.optiontoggle').click(function(event){
+    //alert("Clicked!");
+    $('#resultoptions').toggle();
+    // $('#resultvids').css('border-left', '1px #bbf dotted')
+  });
+
+})
